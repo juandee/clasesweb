9ac3394ec18a59003950a9fd56d18bccf07a4642
Juan Desiderio <juan_nac05@hotmail.com>
2020-04-17 15:17:10 -0300

    arreglo edición contraseña con devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,7 @@ class ApplicationController < ActionController::Base
 
         def configure_permitted_parameters
             devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :surname, :dni, :birthday, :email])
+            update_attrs = [:password, :password_confirmation, :current_password]
+    		devise_parameter_sanitizer.permit :account_update, keys: update_attrs
         end
 end
